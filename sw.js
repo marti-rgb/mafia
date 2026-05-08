e479572fc2e6f0491771191e6b4cfc6f7c47a376
marti-rgb <megamartish@gmail.com>
2026-05-08 03:10:35 +0300

    ver 12
    sw

--- sw.js
+++ sw.js
@@ -1,4 +1,13 @@
-const CACHE = 'mafia-v1';
-const FILES = ['./index.html', './manifest.json'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE = 'mafia-v3';
+const STATIC = ['./manifest.json'];
+
+self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC))); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
+
+self.addEventListener('fetch', e => {
+  if(e.request.url.endsWith('/') || e.request.url.endsWith('index.html')){
+    e.respondWith(fetch(e.request).then(r => { caches.open(CACHE).then(c => c.put(e.request, r.clone())); return r; }).catch(() => caches.match(e.request)));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
